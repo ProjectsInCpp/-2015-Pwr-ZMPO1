21e556aeba7a6acb6b3fd722760c9201a08b9225
korytkoBT <209826@student.pwr.wroc.pl>
2015-12-13 17:06:55 +0000

    filtering Wrong Chars For Some PositionalValues Works, now Testing Further And Dividing Onto Decimal And Binary

--- Values/ValuesLibrary/Positional.h
+++ Values/ValuesLibrary/Positional.h
@@ -7,12 +7,33 @@ class CPositional :
 {
 	CAlphabet* _alfa=nullptr;
 
+	vector<int>* replaceWrongWithZero(vector<int>* value)
+	{
+		vector<int>* retVal = new vector<int>(value->begin(),value->end());
+
+		for (vector<int>::iterator it = retVal->begin();it != retVal->end();++it)
+		{
+			bool res = find(_alfa->GetVal()->begin(),
+				_alfa->GetVal()->end(),
+				*it) != _alfa->GetVal()->end();
+
+			if(!res)
+			{
+				*it = 0;
+			}
+		}
+
+		delete value;
+		return retVal;
+	}
+
 public:
 
 	CPositional(char base, const string& val, char sign)
 		: IValue(base, val, sign)
 	{
 		_alfa = new CAlphabet(this->GetBase());
+		this->_val = replaceWrongWithZero(this->_val);
 	}
 
 	virtual ~CPositional()
--- Values/ValuesTests/CPositional_Tests.cpp
+++ Values/ValuesTests/CPositional_Tests.cpp
@@ -36,5 +36,77 @@ namespace ValuesTests
 			Assert::IsTrue(expectedBool & reachedBool);
 		}
 
+		TEST_METHOD(CTOR_PLUS_9A5_10)
+		{
+			// arrange	
+			char base = 'A';
+			string chain = "9C5";
+			char sign = '0';
+			IValue* decimal = new CPositional(base, chain, sign);
+
+			vector<int>* expected = new vector<int>();
+			bool expectedBool = true;
+			bool reachedBool = true;
+			expected->push_back(9);
+			expected->push_back(0);
+			expected->push_back(5);
+
+			// act
+			vector<int>* reached = decimal->GetVal();
+
+			// assert
+			reachedBool = NUtils::AreVOIEquals(expected, reached);
+
+			Assert::IsTrue(expectedBool & reachedBool);
+		}
+
+		TEST_METHOD(CTOR_PLUS_745_8)
+		{
+			// arrange	
+			char base = '8';
+			string chain = "745";
+			char sign = '0';
+			IValue* decimal = new CPositional(base, chain, sign);
+
+			vector<int>* expected = new vector<int>();
+			bool expectedBool = true;
+			bool reachedBool = true;
+			expected->push_back(7);
+			expected->push_back(4);
+			expected->push_back(5);
+
+			// act
+			vector<int>* reached = decimal->GetVal();
+
+			// assert
+			reachedBool = NUtils::AreVOIEquals(expected, reached);
+
+			Assert::IsTrue(expectedBool & reachedBool);
+		}
+
+		TEST_METHOD(CTOR_PLUS_BA3_16)
+		{
+			// arrange	
+			char base = 'G';
+			string chain = "BA3";
+			char sign = '0';
+			IValue* decimal = new CPositional(base, chain, sign);
+
+			vector<int>* expected = new vector<int>();
+			bool expectedBool = true;
+			bool reachedBool = true;
+			expected->push_back(11);
+			expected->push_back(10);
+			expected->push_back(3);
+
+			// act
+			vector<int>* reached = decimal->GetVal();
+
+			// assert
+			reachedBool = NUtils::AreVOIEquals(expected, reached);
+
+			Assert::IsTrue(expectedBool & reachedBool);
+		}
+
 	};
 }
