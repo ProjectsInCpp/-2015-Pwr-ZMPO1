7e890b7c895d6cdd03c843b0834908f733d3f098
korytkoBT <209826@student.pwr.wroc.pl>
2015-12-14 14:26:13 +0000

    Adding Unsigned UnAligned Values Works, now Sign , after Multiply

--- Values/ValuesLibrary/Add.h
+++ Values/ValuesLibrary/Add.h
@@ -9,14 +9,38 @@ public:
 	virtual CPositional* apply(CPositional* first, CPositional* second)
 	{
 		vector<int>* retVal = new vector<int>();
-		int base = first->GetIntBase();
-		int sign = 0;
+		vector<int>* firstCopy = new vector<int>();
+		vector<int>* secondCopy = new vector<int>();
 
-		vector<int>::reverse_iterator Xi = first->GetVal()->rbegin();
-		vector<int>::reverse_iterator Yi = second->GetVal()->rbegin();
+		int base = first->GetIntBase();
+		char sign = '0';
+
+		int sizeSub = first->GetVal()->size() - second->GetVal()->size();
+
+		if (sizeSub > 0)
+		{
+			firstCopy->resize(first->GetVal()->size());
+			secondCopy->resize(first->GetVal()->size());
+		}
+		else if (sizeSub < 0)
+		{
+			firstCopy->resize(second->GetVal()->size());
+			secondCopy->resize(second->GetVal()->size());
+		}
+		else
+		{
+			firstCopy->resize(first->GetVal()->size());
+			secondCopy->resize(second->GetVal()->size());
+		}
+
+		transform(first->GetVal()->rbegin(), first->GetVal()->rend(), firstCopy->rbegin(), [](char a) { return a; });
+		transform(second->GetVal()->rbegin(), second->GetVal()->rend(), secondCopy->rbegin(), [](char a) { return a; });
+
+		vector<int>::reverse_iterator Xi = firstCopy->rbegin();
+		vector<int>::reverse_iterator Yi = secondCopy->rbegin();
 		int Ciplus1 = 0;
 
-		while (Xi != first->GetVal()->rend()) {
+		while (Xi != firstCopy->rend()) {
 			retVal->push_back((*Xi + *Yi + Ciplus1)% base);
 			Ciplus1 = (*Xi++ + *Yi++ + Ciplus1) / base;
 		}
@@ -29,6 +53,9 @@ public:
 		string* retStr = new string(retRev->begin(),retRev->end());
 
 		delete retVal;
+		delete first;
+		delete second;
+
 		CPositional* end = new CPositional(NUtils::ToEqualsChar(base), *retStr, NUtils::ToEqualsInt(sign));
 		return end;
 	}
